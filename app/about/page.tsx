a6369c4532b53304c7a8504929cb4c61bbd9c204
Nur lay <pmiisttikominsanunggul@gmail.com>
2024-12-16 18:59:02 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>About Page</h1>
+      <a href="http://localhost:3000">Home Page</a>
+    </div>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import HelloWorld from "@/components/HelloWorld";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <h1 className="text-2xl font-bold">Home Page</h1>
+    <a href="http://localhost:3000/about"className="text-blue-500 underline">About Page</a>
+    </div>
+  );
+}
